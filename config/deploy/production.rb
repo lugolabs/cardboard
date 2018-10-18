2791fbaa70042a5aca84daea5c7174fe296171e1
Artan Sinani <artisinani@gmail.com>
2018-10-18 17:47:26 +0200

    Remove Capistrano

--- config/deploy/production.rb
+++
@@ -1,3 +0,0 @@
-set :stage, :production
-set :rails_env, 'production'
-server fetch(:website_url), user: fetch(:deploy_user), roles: %w[web app db]
--- config/deploy/server.example.rb
+++
@@ -1,5 +0,0 @@
-set :deploy_user,     '[YOUR_DEPLOY_USER]'
-set :website_url,     '[YOUR_WEBSITE_URL]'
-set :enable_ssl,      true
-set :ssl_certificate, '[YOUR_SSL_CERTIFICATE]'
-set :ssl_key,         '[YOUR_SSL_KEY]'
